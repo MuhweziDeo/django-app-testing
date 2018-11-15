c10277fa97913d99815ad33122cfa68135ecc853
muhwezideo <aggrey256@gmail.com>
2018-11-15 13:22:42 +0300

    refactor coder

--- testing/products/tests/test_views.py
+++ testing/products/tests/test_views.py
@@ -1,27 +1,32 @@
 from django.test import RequestFactory
 from django.urls import reverse
-from django.contrib.auth.models import User,AnonymousUser
+from django.contrib.auth.models import User, AnonymousUser
 from products.views import product_detail
 from mixer.backend.django import mixer
 import pytest
 from products.models import Product
+from django.test import TestCase
 
 
 @pytest.mark.django_db
-class TestViews:
+class TestViews(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super(TestViews, cls).setUpClass()
+        mixer.blend(Product)
+        cls.factory = RequestFactory()
 
     def test_product_view_authenticated(self):
-        mixer.blend(Product)
         path = reverse('detail', kwargs={'pk': 1})
-        request = RequestFactory().get(path)
+        request = self.factory.get(path)
         request.user = mixer.blend(User)
         response = product_detail(request, pk=1)
         assert response.status_code == 200
 
     def test_product_view_unathenticated(self):
-        mixer.blend(Product)
         path = reverse('detail', kwargs={'pk': 1})
-        request = RequestFactory().get(path)
+        request = self.factory.get(path)
         request.user = AnonymousUser()
         response = product_detail(request, pk=1)
         assert response.status_code == 302
