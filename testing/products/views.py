2f4f23b724207570d9661bb6546b7d517aac3322
muhwezideo <aggrey256@gmail.com>
2018-11-15 11:42:14 +0300

    create model, view, url pattern and template

--- testing/products/views.py
+++ testing/products/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,get_object_or_404
+from . import models
 # Create your views here.
+def product_detail(request,pk):
+	product=get_object_or_404(models.Product,pk=pk)
+	return render(request,'product_detail.html',{'product':product})
